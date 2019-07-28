e2dc298383643343275b2e7b7bd485c819cd7e9d
jsloop <jsloop42@gmail.com>
2019-07-28 11:59:40 +0530

    Update bundle identifier

--- ArtStationBot/ArtStationBotTests/ArtStationBotTests.swift
+++ ArtStationBot/ArtStationBotTests/ArtStationBotTests.swift
@@ -3,7 +3,6 @@
 //  ArtStationBotTests
 //
 //  Created by jsloop on 19/07/19.
-//  Copyright © 2019 DreamLisp. All rights reserved.
 //
 
 import XCTest
--- ArtStationBot/ArtStationBotUITests/ArtStationBotUITests.swift
+++ ArtStationBot/ArtStationBotUITests/ArtStationBotUITests.swift
@@ -3,7 +3,6 @@
 //  ArtStationBotUITests
 //
 //  Created by jsloop on 19/07/19.
-//  Copyright © 2019 DreamLisp. All rights reserved.
 //
 
 import XCTest
